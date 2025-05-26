1a79300a616d78196fbf73a86212795c96e37246
Daan <91228994+FireIce7@users.noreply.github.com>
2025-05-26 16:31:54 -0300

    Correção DB

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import streamlit as st
 from textos import TEXTOS
 from calculadora import show_price_calculator
 from producao import show_production_costs
+from banco import init_db
+
+# Inicializar banco de dados
+init_db()
 
 # Configuração da página
 st.set_page_config(page_title="Custos do Ecommerce", layout="wide")
